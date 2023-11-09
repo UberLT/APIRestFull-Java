717dada3554b496d01907b1839092f2a5593b63f
Uberlan Torres <uberlan9@gmail.com>
2023-11-09 19:09:12 -0300

    Obtendo um post por id

---
+++ workshopmongo/src/main/java/com/uberTecnologs/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.uberTecnologs.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.uberTecnologs.workshopmongo.domain.Post;
+import com.uberTecnologs.workshopmongo.repository.PostRepository;
+import com.uberTecnologs.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		}
+}
